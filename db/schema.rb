f4ddb6b73d3cf804a80cca7f9c0c95071d59a5f8
quocle <quocle@example_not_email.org>
2012-06-17 14:26:22 -0400

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120617021459) do
+ActiveRecord::Schema.define(:version => 20120617043730) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(:version => 20120617021459) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "username"
     t.string   "last_name"
     t.string   "first_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
