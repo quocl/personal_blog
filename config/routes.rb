933ec0f11ec82dc758230877905b1b553b1658e4
quocle <quocle@example_not_email.org>
2012-06-17 00:45:59 -0400

    change home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ PersonalBlog::Application.routes.draw do
     resources :comments
   end
   get "/home" => "posts#posts_detail", :as => 'posts_detail'
-
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,7 +57,8 @@ PersonalBlog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'posts#posts_detail'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
