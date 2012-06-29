db82b1db995d578a6ef063980a7346ae0e9485bf
quocle <quocle@example_not_email.org>
2012-06-29 18:34:19 -0400

    add request spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,45 @@
 require 'spec_helper'
+include Warden::Test::Helpers
 
 describe "Posts" do
-  describe "GET /posts" do
-    it "should get post" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      pending "TODO"
+  describe "Not Sign in" do
+    describe "GET /posts" do
+      before(:each) do
+        visit '/posts'
+      end
+      it "should get posts" do
+        # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+        current_path.should eq('/posts')
+      end    
+      ["Title", "Created At"].each do |field|
+        it "should show field \"#{field}\" of posts page" do
+          page.should have_content(field)
+        end
+      end
+      it "should not show \"Actions\" on posts page" do
+        page.should_not have_content("Actions")  
+      end
+    end  
+  end
+  
+  describe "Signed in" do
+    let!(:user){FactoryGirl.create(:user)}
+    before(:each) do
+      login_as user, :scope => :user
+      visit '/posts'
+    end
+    after(:each) do
+      logout(user)
+    end
+    describe "GET /posts" do
+      it "should get posts" do
+        current_path.should eq("/posts")
+      end
+      ["Title", "Created At", "Actions"].each do |field|
+        it "should have content \"#{field}\"" do
+            page.should have_content(field)    
+        end
+      end
     end
   end
 end
