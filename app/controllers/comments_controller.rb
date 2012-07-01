e98d2ab9d670e29336082f6a1e6a04504046504c
quocle <quocle@example_not_email.org>
2012-07-01 11:33:52 -0400

    add destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,16 @@ class CommentsController < ApplicationController
        format.js
    end
   end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    if @post.user == current_user
+      @comment.destroy
+    end
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.json { head :no_content }
+    end
+  end
 end
