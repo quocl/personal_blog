3f8f4e834169d9f84c57dd7344f4bb65c2f74811
quocle <quocle@example_not_email.org>
2013-01-31 21:53:58 -0500

    More controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -28,6 +28,10 @@ describe PostsController do
       it "should assign post to post" do
         assigns[:post].should eq(post)
       end
+      
+      it "should render show template" do
+        response.should render_template("show")
+      end
     end
     
     describe 'GET posts_detail' do
@@ -50,29 +54,54 @@ describe PostsController do
         response.should redirect_to(user_session_path)  
       end
     end
-    
-    describe 'GET show' do
-      it "should show the post" do
-        get :show, :id => post.id
-        assigns(:post).should eq(post)
-      end
-    end
   end
   
   describe "Login" do
-    let!(:user){FactoryGirl.create(:user)}
+    let!(:post){ FactoryGirl.create(:post)}
     before(:each) do
-      sign_in user
+      sign_in post.user
     end
       
     after(:each) do
-      sign_out user
+      sign_out post.user
     end
   
     describe "GET new" do
       it "should create a un-saved post" do
         expect{get :new}.to change{Post.count}.by(0)
         assigns[:post].should_not be_persisted
+      end
+    end
+    
+    describe "GET edit" do
+      it "should render template edit" do
+        get :edit, :id => post
+        response.should render_template("edit")
+      end
+    end
+    
+    describe "DELETE destroy" do
+      it "should redirect to posts_path" do
+        delete :destroy, :id => post
+        response.should redirect_to(posts_path)
+      end
+      
+      it "should destroy the post" do
+        expect{delete :destroy, :id => post}.to change{Post.count}.by(-1)
+      end
+    end
+    
+    describe "PUT update" do
+      before(:each) do
+        put :update, :id => post, :post => {:content => "new content", :title => "new_title"}
+      end
+      
+      it "should assign to post" do
+        assigns[:post].should eq(post)
+      end
+      
+      it "should redirect to posts_path" do
+        response.should redirect_to("/posts/new_title")
       end
     end
   end
